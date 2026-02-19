8649d645ded62ebe9f973a76bb36f658bb6ec085
Luca Del Puppo <Puppo@users.noreply.github.com>
2026-02-19 23:09:17 +0100

    feat: add ESLint configuration and scripts for linting

---
+++ eslint.config.ts
@@ -0,0 +1,21 @@
+import js from "@eslint/js";
+import markdown from "@eslint/markdown";
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: { tseslint, js },
+    languageOptions: { globals: globals.node },
+  },
+  {
+    files: ["**/*.md"],
+    plugins: { markdown },
+    language: "markdown/gfm",
+    rules: {
+      "no-irregular-whitespace": "off",
+    },
+  },
+]);
